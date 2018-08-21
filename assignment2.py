9a8b3ba66e6ff1cf5013c2ec4ce544f28ae23748
pushpinder12345 <pushpinderosan12345@gmail.com>
2018-08-21 10:15:41 +0530

    This is my second assignment

---
+++ assignment2.py
@@ -0,0 +1,46 @@
+#Q1) Print anything you want on screen.
+print("this is to print anything on screen ")
+
+#Q2) Join two strings using '+' .
+'''e.g.-"Acad" + "View" '''
+
+str='Acad'
+str1 ='View'
+str3=str+str1
+print(str3)
+
+#Q3) Take the input of 3 variables x,y and z. Printt theri values on screen.
+x=int(input("Enter First Element "))
+y=input("Enter Second Element")
+z=input("Enter third Element")
+print(x)
+print(y)
+print(z)
+
+#Q4) Print"Let 's get started " on screen.
+print("Let' s get started")
+
+
+#Q5) Print the following values using placeholders.
+'''s="Acadview"
+course="python"
+fees=5000 '''
+
+
+s="Acadview"
+course="python"
+fees=5000
+
+print('%s' '%s' '%d' %(s,course,fees))
+
+#Q6) Find the area of circle
+'''pi = 3.14
+take radius as input from user
+print the area of circle '''
+
+
+radius=int(input("Enter the value of Radius"))
+pi=3.14
+area=pi*radius*radius
+print(area)
+  
